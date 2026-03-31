8ac773e42a35bd0873ab9fe8d1939ac16270a3b9
Robbie Brush <rctbrush@gmail.com>
2026-03-31 13:12:01 -0400

    Added environment variable as port for deploying in index.js

--- server/index.js
+++ server/index.js
@@ -19,8 +19,8 @@ app.use("/tasks", tasksRouter);
 
 
 db.sequelize.sync().then(()=> {
-    app.listen(3001, () => {
-        console.log("Server is running on port 3001.");
+    app.listen(process.env.PORT || PORT, () => {
+        console.log(`Server is running on port ${PORT}.`);
     });
 });
 
